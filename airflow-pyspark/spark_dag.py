f029b8924ea4fac6947fc8f95ad632526bd0611d
Sohail-github123 <150891088+Sohail-github123@users.noreply.github.com>
2024-03-14 20:45:47 +0530

    Add files via upload

---
+++ airflow-pyspark/spark_dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime ,timedelta
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 3, 15),
+}
+
+dag = DAG(
+    "pyspark_dag",
+    default_args=default_args,
+    description="simple pyspark",
+    schedule_interval=None,
+)
+
+csv_to_parquet = BashOperator(
+    task_id="csv_to_parquet",
+    bash_command="spark-submit ./spark_jobs/csv_to_parquet.py ",
+    dag=dag,
+)
+
+add_columns= BashOperator(
+    task_id="add_columns",
+    bash_command="spark-submit ./spark_jobs/add_column.py ",
+    dag=dag,
+)
+
+csv_to_parquet >> add_columns
